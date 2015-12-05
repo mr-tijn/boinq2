12edb320727c6c95015bcce4a991d4785e345c73
Kleurenprinter <jimclauwaert@gmail.com>
2015-12-05 13:05:40 +0100

    allign Proxyconfiguration

--- src/main/java/com/genohm/boinq/config/ProxyConfiguration.java
+++ src/main/java/com/genohm/boinq/config/ProxyConfiguration.java
@@ -51,9 +51,9 @@ public class ProxyConfiguration implements EnvironmentAware {
 	}
 
 	private class EldaProxyServlet extends ProxyServlet.Transparent {
-		
-		/**		
-		 * 		
+
+		/**
+		 * 
 		 */
 		private static final long serialVersionUID = 1L;
 		// two ProxyServlets of same type won't work together
