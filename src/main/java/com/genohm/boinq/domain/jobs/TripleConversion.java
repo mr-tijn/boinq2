70e8a32549ff3e35c11ee76fe5e54734c2ee5ea4
Kleurenprinter <jimclauwaert@gmail.com>
2016-04-22 13:56:27 +0200

    Fix to type selector

--- src/main/java/com/genohm/boinq/domain/jobs/TripleConversion.java
+++ src/main/java/com/genohm/boinq/domain/jobs/TripleConversion.java
@@ -152,10 +152,14 @@ public class TripleConversion implements AsynchronousJob {
 			meta.prefixLength = (track.getContigPrefix()==null)? 0:track.getContigPrefix().length();
 			Iterator<Triple> tripleIterator = tripleIteratorFactory.getIterator(inputFile, track.getReferenceMap(), meta);
 			if(meta.fileType.equals("bed") && track.getType()!=null){
+			Map<String, Node> referenceMap = getReferenceMap(track);
+			Iterator<Triple> tripleIterator = tripleIteratorFactory.getIterator(inputFile, referenceMap, meta);
+				if(meta.fileType.equals("bed") && track.getType()!=null){
 				String[] types=track.getType().split("\\|");
 				meta.mainType= NodeFactory.createURI(types[0]);
 				meta.typeList.add(meta.mainType);
-				if (types[1]!=null){
+				int test = types.length;
+				if (types.length==2){
 					meta.subType = NodeFactory.createURI(types[1]);
 					meta.typeList.add(meta.subType);
 				}
